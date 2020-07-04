87f6d88523bc3b092089a5749c1781a33f05722f
Nikolay.L <roner1kk@gmail.com>
2020-07-04 19:39:17 +0300

    add structure prj

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AppHeader = () => {
+    return (
+        <div className='app-header d-flex'>
+            <h1>Nikolay Prosto</h1>
+            <h2>5 записей, и 10 понравилось</h2>
+        </div>
+    )
+}
+export default AppHeader;
