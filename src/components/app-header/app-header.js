224fccd26c22ba044e0184cca0cb499542fc1255
Nikolay.L <roner1kk@gmail.com>
2020-07-07 01:31:53 +0300

    complete

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,25 +1,25 @@
 import React from "react";
 import './app-header.css'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 
-const Header = styled.div`
- display:flex;
- align-items:flex-end;
- justify-content:space-between;
-  h1{
- font-size:26px;
- color: ${props => props.colored ? 'red' : 'black'};
-  :hover{
-  color:blue;
-  }
- }
-`
-const AppHeader = () => {
+// const Header = styled.div`
+//  display:flex;
+//  align-items:flex-end;
+//  justify-content:space-between;
+//   h1{
+//  font-size:26px;
+//  color: ${props => props.colored ? 'red' : 'black'};
+//   :hover{
+//   color:blue;
+//   }
+//  }
+// `
+const AppHeader = ({liked,allPosts}) => {
     return (
-        <Header as='header' >
+        <div className='app-header d-flex' >
             <h1>Nikolay Prosto</h1>
-            <h2>5 записей, и 10 понравилось</h2>
-        </Header>
+            <h2>{allPosts} записей, и {liked} понравилось</h2>
+        </div>
     )
 }
 export default AppHeader;
